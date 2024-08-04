1f5e2658551b0a4f654498c06c50b500224fbdc6
Tushar Nikam <champ96k@gmail.com>
2024-08-04 16:53:13 +0530

    Create movie_details_state.dart

---
+++ lib/features/book_movies_ticket/presentation/movie_details_cubit/movie_details_state.dart
@@ -0,0 +1,8 @@
+// part of 'movie_details_cubit.dart';
+
+// class MoviesDetailsLoadedState extends BaseState {
+//   MoviesDetailsLoadedState(this.movieDetailsModel);
+//   final MovieDetailsModel? movieDetailsModel;
+//   @override
+//   List<Object?> get props => [movieDetailsModel];
+// }
