f3b94574347a85ead31ea2aec29560e1fdc0b15e
Tushar Nikam <champ96k@gmail.com>
2024-09-16 16:56:45 +0530

    Create core_services.dart

---
+++ lib/core/services/core_services.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+
+class CoreServices {
+  int x = 0;
+  int ydnjdhbdf = 0;
+
+  final StreamController<int> _controller = StreamController<int>();
+
+  dynamic temp = 12;
+  void initialize() {
+    print("initializing Core services");
+    _emptySetupHive();
+  }
+
+  void _emptySetupHive() {
+    temp = "Hive Initialized ⛑";
+  }
+
+  _init(int value) {
+    _controller.add(value);
+  }
+}
