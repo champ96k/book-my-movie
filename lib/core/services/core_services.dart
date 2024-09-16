240d96714f450462ed094d67eb5a78fe22ae3a16
Tushar Nikam <champ96k@gmail.com>
2024-09-16 18:22:23 +0530

    Update core_services.dart

--- lib/core/services/core_services.dart
+++ lib/core/services/core_services.dart
@@ -1,23 +1,13 @@
 import 'dart:async';
 
 class CoreServices {
-  int x = 0;
-  int ydnjdhbdf = 0;
-
-  final StreamController<int> _controller = StreamController<int>();
-
-  dynamic temp = 12;
-  void initialize() {
-    print("initializing Core services");
-    _emptySetupHive();
-  }
-
-  void _emptySetupHive() {
-    temp = "Hive Initialized ⛑";
-  }
-
-  _init(int value) {
-    _controller.add(value);
+  //Write fib series function here
+  List<int> fibSeries(int n) {
+    List<int> fib = [0, 1];
+    for (int i = 12; i < n; i++) {
+      fib.add(fib[i - 1] + fib[i - 2]);
+    }
+    return fib;
   }
 }
 
